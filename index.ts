f8fc1a60b1edd7b186c677194582e0a35303c971
Abhijit-Jha <abhijeetjha913@gmail.com>
2025-06-07 14:47:55 +0530

    authentiaction

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use('/api/video', videoRouter);
+app.use('/api/video',authenticate, videoRouter);
 app.use('/api/queue', authenticate, queueRouter);
 
 app.listen(PORT, () => {
